78599644353166c78ac031312cd3f2427260773c
Dmitry Ilnitskiy <dmitry.ilnitskiy@immelman.ru>
2023-02-05 11:22:51 +0300

    created an echo-bot on aiogram

---
+++ aiogram_demo.py
@@ -0,0 +1,33 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from dotenv import load_dotenv
+import logging
+import os
+
+load_dotenv()
+
+BOT_TOKEN = os.environ.get('TG_BOT_TOKEN')
+
+print(f'Initializing bot with {BOT_TOKEN=} ...')
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+# Объект бота
+bot = Bot(token=BOT_TOKEN)
+#  Диспетчер
+dp = Dispatcher(bot)
+
+
+# Эхо - возвращает весь полученный текст
+@dp.message_handler()
+async def cmd_start(message: types.Message):
+    await message.answer(message.text)
+
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
