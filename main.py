52e2c4ffc8ae6bc9e191a190e4ed3062a63b3ff8
n9dmitry <121239886+n9dmitry@users.noreply.github.com>
2023-02-05 16:26:36 +0300

    v0.05

--- main.py
+++ main.py
@@ -1,55 +1,36 @@
-import telebot
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from dotenv import load_dotenv
+import logging
+import os
+import secrets
 
-TOKEN = "6095088518:AAFKVIpJqGLaWJLyNDGp8L2SHhjLToBvE9w"
-bot = telebot.TeleBot(TOKEN)
+load_dotenv()
 
+BOT_TOKEN = secrets.BOT_TOKEN
+# BOT_TOKEN = os.environ.get('TG_BOT_TOKEN')
+print(BOT_TOKEN)
 
+print(f'Initializing bot with {BOT_TOKEN=} ...')
 
-# Приветствие. Опрос: фио, возраст, род занятий.
-
-def process_survey(message):
-    try:
-        full_name = message.text.strip()
-        msg = bot.send_message(chat_id=message.chat.id,
-                               text="Сколько лет тебе?")
-        bot.register_next_step_handler(msg, process_age, full_name=full_name)
-    except Exception as e:
-        bot.reply_to(message, 'Упс. Ошибка. Что-то не так...')
-
-def process_age(message, full_name):
-    try:
-        age = message.text.strip()
-        msg = bot.send_message(chat_id=message.chat.id,
-                               text="Какой твой род занятий")
-        bot.register_next_step_handler(msg, process_occupation, full_name=full_name, age=age)
-    except Exception as e:
-        bot.reply_to(message, 'Упс. Ошибка. Что-то не так...')
-
-def process_occupation(message, full_name, age):
-    try:
-        occupation = message.text.strip()
-        bot.send_message(chat_id=message.chat.id,
-                         text="Спасибо, {}! Тебе {} лет твой род занятий {}.".format(full_name, age, occupation))
-    except Exception as e:
-        bot.reply_to(message, 'Упс. Ошибка. Что-то не так...')
-
-def confirm_survey(msg):
-    if msg.text.lower() == '+':
-        bot.send_message(chat_id=msg.chat.id, text='Как зовут тебя, о юный отрок? (ФИО развернуто) ')
-        bot.register_next_step_handler(msg, process_survey)
-
-    else:
-        bot.send_message(chat_id=msg.chat.id, text='Не верно! Напиши "+" чтобы ответить на вопросы ')
-        bot.register_next_step_handler(msg, confirm_survey)
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+# Объект бота
+bot = Bot(token=BOT_TOKEN)
+#  Диспетчер
+dp = Dispatcher(bot)
 
 
-@bot.message_handler(commands=['start'])
-def start_handler(message):
-    bot.send_message(chat_id=message.chat.id, text="Привет! Важно!!")
-    bot.send_message(chat_id=message.chat.id, text="У нас обязательный опросник (3 вопроса). Все участники должны его заполнить или бот автоматически кикнет из группы в течении 24 часов. Напиши '+' без кавычик чтобы пройти ")
-    bot.register_next_step_handler(message, confirm_survey)
+# Эхо - возвращает весь полученный текст
+@dp.message_handler()
+async def cmd_start(message: types.Message):
+    await message.answer(message.text)
+
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
-    bot.polling()
-
-#
+    asyncio.run(main())
