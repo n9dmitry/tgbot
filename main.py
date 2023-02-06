edafeeab561ca5612b016004f3c20977d2e72085
n9dmitry <121239886+n9dmitry@users.noreply.github.com>
2023-02-07 01:20:43 +0300

    v0.07

--- main.py
+++ main.py
@@ -1,44 +1,84 @@
-import asyncio
-from aiogram import Bot, Dispatcher, executor, types
-from dotenv import load_dotenv
 import logging
 import os
 
+from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.utils import executor
+from dotenv import load_dotenv
+
+logging.basicConfig(level=logging.INFO)
 load_dotenv()
 
-# BOT_TOKEN = secrets.BOT_TOKEN
-# BOT_TOKEN = '6095088518:AAFKVIpJqGLaWJLyNDGp8L2SHhjLToBvE9w'
-BOT_TOKEN = os.environ.get('TG_BOT_TOKEN')
+API_TOKEN = os.environ.get('TG_BOT_TOKEN')
 
-# text = "Важно!! 🧨 \nУ нас обязательный опросник (3 вопроса). \n 🎫 Все участники должны его заполнить или бот автоматически кикнет из группы в течении 24 часов. Напиши '+' без кавычик чтобы пройти. \nHi there! What's your name?"
+bot = Bot(token=API_TOKEN)
 
-print(f'Initializing bot with {BOT_TOKEN=} ...')
+# For example use simple MemoryStorage for Dispatcher.
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
 
-# Включаем логирование, чтобы не пропустить важные сообщения
-logging.basicConfig(level=logging.INFO)
-# Объект бота
-bot = Bot(token=BOT_TOKEN)
-#  Диспетчер
-dp = Dispatcher(bot)
+
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+
+
+# States
+class Form(StatesGroup):
+    confirm = State() # +
+    name = State()  # Will be represented in storage as 'Form:name'
+    age = State()  # Will be represented in storage as 'Form:age'
+    occupation = State()  # Will be represented in storage as 'Form:occupation'
+
 
 @dp.message_handler(commands='start')
 async def cmd_start(message: types.Message):
-    await message.answer("Привет, как дела?")
+    await message.reply("Важно!! 🧨 \nУ нас обязательный опросник (3 вопроса). \n 🎫 Все участники должны его заполнить или бот автоматически кикнет из группы в течении 24 часов. Напиши '+' без кавычик чтобы пройти.")
+    await Form.next()
 
-@dp.message_handler()
-async def echo(message: types.Message):
-    await message.answer("У меня всё хорошо, спасибо что спросил")
+@dp.message_handler(state=Form.confirm)
+async def confirm(message: types.Message, state: FSMContext):
+    if message.text == "+":
+        async with state.proxy() as data:
+            data['confirm'] = message.text
+        await Form.next()
+        await message.reply("Всего 3 вопроса. Вопрос 1: Напишите ФИО полностью")
+    else:
+        await message.answer("Нет")
 
-#
-# Запуск процесса поллинга новых апдейтов
-# async def main():
-#     await dp.start_polling(bot)
-#
-#
-# if __name__ == "__main__":
-#     asyncio.run(main())
+@dp.message_handler(state=Form.name)
+async def ask1_name(message: types.Message):
+    if not len(message.text.split()) >= 2 and len(message.text.split()) <= 3:
+        return await message.reply("Шляпа коня")
+    else:
+        await Form.next()
+        return await message.reply("Сколько тебе лет?")
+
+
+@dp.message_handler(state=Form.age)
+async def ask2_age(message: types.Message):
+    try:
+        if 10 < int(message.text) < 80:
+            await Form.next()
+            return await message.reply("Какой твой род деятельности?")
+        else:
+            return await message.reply("Лох")
+    except:
+        return await message.reply('dolboeb')
+
+@dp.message_handler(state=Form.occupation)
+async def ask3_occupation(message: types.Message):
+    try:
+        if len(message.text) < 150:
+            await Form.next()
+            return await message.reply("Ок, молодец")
+        else:
+            return await message.reply("Короче давай пиши")
+    except:
+        return await message.reply('dolboeb')
+
 
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
-
