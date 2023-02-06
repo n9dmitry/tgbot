888e42595934c4cc625a40defcacd0cc048c8373
n9dmitry <121239886+n9dmitry@users.noreply.github.com>
2023-02-06 12:50:13 +0300

    v0.06

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import asyncio
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, executor, types
 from dotenv import load_dotenv
 import logging
 import os
-import secrets
 
 load_dotenv()
 
-BOT_TOKEN = secrets.BOT_TOKEN
-# BOT_TOKEN = os.environ.get('TG_BOT_TOKEN')
-print(BOT_TOKEN)
+# BOT_TOKEN = secrets.BOT_TOKEN
+# BOT_TOKEN = '6095088518:AAFKVIpJqGLaWJLyNDGp8L2SHhjLToBvE9w'
+BOT_TOKEN = os.environ.get('TG_BOT_TOKEN')
+
+# text = "Важно!! 🧨 \nУ нас обязательный опросник (3 вопроса). \n 🎫 Все участники должны его заполнить или бот автоматически кикнет из группы в течении 24 часов. Напиши '+' без кавычик чтобы пройти. \nHi there! What's your name?"
 
 print(f'Initializing bot with {BOT_TOKEN=} ...')
 
@@ -20,17 +21,24 @@ bot = Bot(token=BOT_TOKEN)
 #  Диспетчер
 dp = Dispatcher(bot)
 
-
-# Эхо - возвращает весь полученный текст
-@dp.message_handler()
+@dp.message_handler(commands='start')
 async def cmd_start(message: types.Message):
-    await message.answer(message.text)
+    await message.answer("Привет, как дела?")
 
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer("У меня всё хорошо, спасибо что спросил")
 
+#
 # Запуск процесса поллинга новых апдейтов
-async def main():
-    await dp.start_polling(bot)
+# async def main():
+#     await dp.start_polling(bot)
+#
+#
+# if __name__ == "__main__":
+#     asyncio.run(main())
 
 
-if __name__ == "__main__":
-    asyncio.run(main())
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
+
